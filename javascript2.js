6ddf97d7f6a19e29a6269526a6fb015b9f4cdf43
Max Rubs <38310511+MaxRubshik@users.noreply.github.com>
2018-06-08 15:22:52 +0300

    Add files via upload

---
+++ javascript2.js
@@ -0,0 +1,12 @@
+window.onload.function(){
+
+var x = document.getElementById("body");
+x.style.backgroundImage = url("https://sun9-1.userapi.com/c840723/v840723501/7b271/JgmSwCHns3U.jpg");
+}
+
+
+//this.onload = ()=>{
+
+//document.body.style.backgroundImage = url("https://images.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FkFzBagbox_c%2Fmaxresdefault.jpg&f=1");
+
+//}
